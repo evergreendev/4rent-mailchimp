4df0a2788a2172c689c9f6f795ddc9b8eedd9433
Joe simpson <joe@egmrc.com>
2024-07-16 13:48:54 -0600

    Initial Commit

---
+++ routes/tags.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const mailchimp = require("@mailchimp/mailchimp_marketing")
+const md5 = require("md5");
+
+/* GET users listing. */
+router.get('/', async function (req, res, next) {
+  mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_KEY,
+    server: "us10"
+  });
+  if (req.query.email) {
+    const email = req.query.email;
+    const listId = "64df6ac5cf";
+    const subscriberHash = md5(email);
+    const tag = req.query.tag === "direct" ? "4 Rent Direct Contact" : "4 Rent Maybe Interested";
+
+    try{
+      await mailchimp.lists.updateListMemberTags(
+          listId,
+          subscriberHash,
+          {
+            tags: [
+                {
+                  name: tag,
+                  status: "active",
+                },
+            ],
+          }
+      );
+    } catch (e){
+      console.log(e);
+    }
+
+
+
+  }
+
+  res.render('index', {title: '4RENT BLACKHILLS'});
+});
+
+module.exports = router;
